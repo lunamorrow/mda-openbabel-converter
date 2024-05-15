331b4eb8f29cb7afe312d8b507601b67787f4b81
lunamorrow <luna.morrow2@gmail.com>
2024-05-15 18:47:29 +1000

    Initial creation with MDAnalysis Cookiecutter 0.1

---
+++ mda_openbabel_converter/__init__.py
@@ -0,0 +1,9 @@
+"""
+mda-openbabel-converter
+A package to convert between MDAnalysis and OpenBabel Objects
+"""
+
+# Add imports here
+from importlib.metadata import version
+
+__version__ = version("mda_openbabel_converter")
