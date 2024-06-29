58ddeaea07597fcac948b30a4d4637f349c62025
lunamorrow <luna.morrow2@gmail.com>
2024-06-29 10:14:36 +1000

    OpenBabelParser and tests work 29 June

--- mda_openbabel_converter/OpenBabelParser.py
+++ mda_openbabel_converter/OpenBabelParser.py
@@ -6,14 +6,38 @@ import MDAnalysis as mda
 from MDAnalysis.topology.base import TopologyReaderBase
 from MDAnalysis.core.topology import Topology
 from MDAnalysis.converters.base import ConverterBase
+from MDAnalysis.core.topologyattrs import (
+    Atomids,
+    Atomnames,
+    Atomtypes,
+    Elements,
+    Masses,
+    Charges,
+    Aromaticities,
+    Bonds,
+    Resids,
+    Resnums,
+    Resnames,
+    RSChirality,
+    Segids,
+    AltLocs,
+    ChainIDs,
+    ICodes,
+    Occupancies,
+    Tempfactors,
+)
 import warnings
+import numpy as np
+HAS_OBABEL=False
 
 try:
-    import openbabel as OB
+    import openbabel as ob
     from openbabel import OBMol
     from openbabel import OBElementTable
+    HAS_OBABEL=True
 except ImportError:
-    print("Cannot find openbabel, install with 'pip install openbabel==2.4.0'")
+    warnings.warn("Cannot find openbabel, install with `mamba install -c "
+                  "conda-forge openbabel`")
 
 class OpenBabelParser(TopologyReaderBase):
     """
@@ -29,12 +53,10 @@ class OpenBabelParser(TopologyReaderBase):
         (i.e., is it a valid OpenBabel OBMol that can be converted to a
         MDAnalysis Topology?)
         """
-        try:
-            import openbabel as OB
-        except ImportError:
+        if HAS_OBABEL == False:
             return False
         else:
-            return isinstance(thing, OB.OBMol)
+            return isinstance(thing, ob.OBMol)
 
     def parse(self, **kwargs):
         """
@@ -44,6 +66,7 @@ class OpenBabelParser(TopologyReaderBase):
         relevant attributes from the OBMol to initialise a new Topology.
         """
         format = 'OPENBABEL'
+        mol = self.filename
 
         self.atoms = []
         self.n_atoms = 0
@@ -72,6 +95,7 @@ class OpenBabelParser(TopologyReaderBase):
         occupancies = []
         tempfactors = []  # B factor; not supported by OB
 
+
         if obmol.GetFirstAtom().equals(None):
             return Topology(n_atoms=0,
                             n_res=0,
@@ -80,7 +104,7 @@ class OpenBabelParser(TopologyReaderBase):
                             atom_resindex=None,
                             residue_segindex=None)
 
-        for atom in OB.OBMolAtomIter(obmol):
+        for atom in ob.OBMolAtomIter(obmol):
             # need to add handling incase attributes are invalid or null in OBMol
             # names.append(atom.GetType()) #char -> nothing for name in OBMol? Is name required to make MDA Atom?
             atomtypes.append(atom.GetType())  # char
@@ -96,22 +120,152 @@ class OpenBabelParser(TopologyReaderBase):
             # convert atomic number to element
             elements.append(OBElementTable.GetSymbol(atom.GetAtomicNumber())) #char
 
-            resid = atom.GetResidue() # null if no residue
-            if resid.equals(None):
-                warnings.warn(
-                    f"No residue is defined for atom (ID: {atom.GetIdx})." 
-                    "Please set with 'SETTTING METHOD FOR OBMOL'" # TO DO
-                )
-                # if residue null, will need to assign w MDAnalysis...
-            else:
+            if atom.HasResidue():
+                resid = atom.GetResidue() # null if no residue
                 resnums.append(resid.GetNum()) # TO DO: check if start at 0 or 1
                 resnames.append(resid.GetName())
-
+                chainids.append(resid.GetChainNum()) # is this correct???
+                icodes.append(resid.GetInsertionCode())
+            else:
+                warnings.warn(
+                    f"No residue is defined for atom (ID: {atom.GetIdx})." 
+                    "Please set with 'MDA SETTING METHOD' if required." # TO DO
+                )
+                resnums.append(None) # TO DO: is this best??
+                resnames.append(None)
+                chainids.append(None)
+                icodes.append(None)
+            
+                 
             # don't need to check null case, as know assigned to OBMol we're currently parsing
             # but, NEED TO HANDLE ADDING MULTIPLE SEGIDS/OBMOLS WHEN CONVERTING TO UNIVERSE AND ADDING TOGETHER... (should be ok, check w tests)
             segids.append(atom.GetParent())
 
-            chiralities.append(atom.IsChiral()) #boolean
+            # TO DO: may need to create seperate for if SMILES input
+            chirality = None
+            if atom.IsPositiveStereo():
+                chirality = "+"
+            if atom.IsNegativeStereo():
+                chirality = "-"
+            chiralities.append(chirality)
+
             aromatics.append(atom.IsAromatic()) #boolean
 
-        pass
+            # altlocs.append()
+            # occupancies.append()
+            # tempfactors.append()
+
+
+        # make Topology attributes
+        attrs = []
+        n_atoms = len(ids)
+
+        if resnums and resnums.__contains__(None):
+            raise ValueError(
+                "ResidueInfo is only partially available in the molecule. "
+                "If you have added hydrogens to the input RDKit molecule with "
+                "`Chem.AddHs(mol)`, consider using "
+                "`Chem.AddHs(mol, addResidueInfo=True)` instead"
+            )
+
+        # * Attributes always present *
+
+        # Atom attributes
+        for vals, Attr, dtype in (
+            (ids, Atomids, np.int32),
+            (elements, Elements, object),
+            (masses, Masses, np.float32),
+            (aromatics, Aromaticities, bool),
+            (chiralities, RSChirality, 'U1'),
+        ):
+            attrs.append(Attr(np.array(vals, dtype=dtype)))
+
+        # # Bonds
+        # bonds = []
+        # bond_types = []
+        # bond_orders = []
+        # for bond in mol.GetBonds():
+        #     bonds.append((bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()))
+        #     bond_orders.append(bond.GetBondTypeAsDouble())
+        #     bond_types.append(str(bond.GetBondType()))
+        # attrs.append(Bonds(bonds, types=bond_types, order=bond_orders))
+
+        # # * Optional attributes *
+
+        # # Atom name
+        # if names:
+        #     attrs.append(Atomnames(np.array(names, dtype=object)))
+        # else:
+        #     for atom in mol.GetAtoms():
+        #         name = "%s%d" % (atom.GetSymbol(), atom.GetIdx())
+        #         names.append(name)
+        #     attrs.append(Atomnames(np.array(names, dtype=object)))
+
+        # # Atom type
+        # if atomtypes:
+        #     attrs.append(Atomtypes(np.array(atomtypes, dtype=object)))
+        # else:
+        #     atomtypes = guessers.guess_types(names)
+        #     attrs.append(Atomtypes(atomtypes, guessed=True))
+
+        # # Partial charges
+        # if charges:
+        #     attrs.append(Charges(np.array(charges, dtype=np.float32)))
+        # else:
+        #     pass # no guesser yet
+
+        # # PDB only
+        # for vals, Attr, dtype in (
+        #     (altlocs, AltLocs, object),
+        #     (chainids, ChainIDs, object),
+        #     (occupancies, Occupancies, np.float32),
+        #     (tempfactors, Tempfactors, np.float32),
+        # ):
+        #     if vals:
+        #         attrs.append(Attr(np.array(vals, dtype=dtype)))
+
+        # # Residue
+        # if any(resnums) and not any(val is None for val in resnums):
+        #     resnums = np.array(resnums, dtype=np.int32)
+        #     resnames = np.array(resnames, dtype=object)
+        #     segids = np.array(segids, dtype=object)
+        #     icodes = np.array(icodes, dtype=object)
+        #     residx, (resnums, resnames, icodes, segids) = change_squash(
+        #         (resnums, resnames, icodes, segids),
+        #         (resnums, resnames, icodes, segids))
+        #     n_residues = len(resnums)
+        #     for vals, Attr, dtype in (
+        #         (resnums, Resids, np.int32),
+        #         (resnums.copy(), Resnums, np.int32),
+        #         (resnames, Resnames, object),
+        #         (icodes, ICodes, object),
+        #     ):
+        #         attrs.append(Attr(np.array(vals, dtype=dtype)))
+        # else:
+        #     attrs.append(Resids(np.array([1])))
+        #     attrs.append(Resnums(np.array([1])))
+        #     residx = None
+        #     n_residues = 1
+
+        # # Segment
+        # if any(segids) and not any(val is None for val in segids):
+        #     segidx, (segids,) = change_squash((segids,), (segids,))
+        #     n_segments = len(segids)
+        #     attrs.append(Segids(segids))
+        # else:
+        #     n_segments = 1
+        #     attrs.append(Segids(np.array(['SYSTEM'], dtype=object)))
+        #     segidx = None
+
+        # create topology
+        # top = Topology(n_atoms, n_residues, n_segments,
+        #                attrs=attrs,
+        #                atom_resindex=residx,
+        #                residue_segindex=segidx)
+
+        top = Topology(n_atoms, 0, 0,
+                       attrs=attrs,
+                       atom_resindex=1,
+                       residue_segindex=1)
+
+        return top
--- mda_openbabel_converter/tests/test_openbabel_parser.py
+++ mda_openbabel_converter/tests/test_openbabel_parser.py
@@ -1,9 +1,9 @@
 # Testing OpenBabel and Pybel
 
 import MDAnalysis as mda
-import openbabel as OB
-from openbabel import OBMol, OBConversion, pybel
-#from pybel import readfile
+import openbabel as ob
+from openbabel import OBMol, OBConversion
+from pybel import readfile
 
 import mda_openbabel_converter
 from mda_openbabel_converter import OpenBabelParser as OBParser
@@ -12,14 +12,19 @@ import sys
 import numpy as np
 from numpy.testing import assert_equal, assert_allclose
 # from mda_openbabel_converter.tests.test_data import # something...
-from MDAnalysisTests.topology.base import ParserBase  # version 2.7.0
+from MDAnalysisTests.topology.base import ParserBase
+
+import mda_openbabel_converter.OpenBabelParser  # version 2.7.0
 
 # *** can run with "python -m pytest" but not "pytest" (can't find 
 # MDAnalysis) - need to fix this! ***
 
 
 class OpenBabelParserBase(ParserBase):
-    parser = OBParser
+    #parser = OBParser
+    parser = mda_openbabel_converter.OpenBabelParser.OpenBabelParser
+    ref_filename = "none"
+
     expected_attrs = ['ids', 'names', 'elements', 'masses', 'aromaticities',
                       'resids', 'resnums', 'chiralities',
                       'segids', 'bonds',
@@ -38,10 +43,11 @@ class OpenBabelParserBase(ParserBase):
         assert len(top.bonds.values) == self.expected_n_bonds
 
 class TestOpenBabelParserEmpty(OpenBabelParserBase):
+    ref_filename = OBMol()
 
-    @pytest.fixture(scope='class')
+    @pytest.fixture(autouse=True)
     def filename(self):
-        return OBMol()
+        return ob.OBMol()
 
     expected_attrs = ['ids', 'names', 'elements', 'masses', 'aromaticities',
                       'resids', 'resnums', 'chiralities',
@@ -49,47 +55,28 @@ class TestOpenBabelParserEmpty(OpenBabelParserBase):
                      ]
     
     expected_n_atoms = 0
-    expected_n_residues = 1
-    expected_n_segments = 1
+    expected_n_residues = 0
+    expected_n_segments = 0
     expected_n_bonds = 0
 
-    @pytest.fixture(scope='class')
-    def topology(self, filename):
-        topology = OBParser(filename)
-        return topology
+    @pytest.fixture(autouse=True)
+    def top(self, filename):
+        top = OBParser.OpenBabelParser(filename)
+        return top
 
-    def test_attributes(self, topology):
+    def test_atoms_total_counts(self, top):
+        assert len(top.select_atoms("all")) == self.expected_n_atoms
 
-        assert len(np.unique(topology.results.hbonds[:, 0])) == 10
-        assert len(topology.results.hbonds) == 32
+    def test_residues_total_counts(self, top):
+        assert len(top.select_atoms("all")) == self.expected_n_atoms
 
-        reference = {
-            'distance': {'mean': 2.7627309, 'std': 0.0905052},
-            'angle': {'mean': 158.9038039, 'std': 12.0362826},
-        }
-
-        assert_allclose(np.mean(h.results.hbonds[:, 4]),
-                        reference['distance']['mean'])
-        assert_allclose(np.std(h.results.hbonds[:, 4]),
-                        reference['distance']['std'])
-        assert_allclose(np.mean(h.results.hbonds[:, 5]),
-                        reference['angle']['mean'])
-        assert_allclose(np.std(h.results.hbonds[:, 5]),
-                        reference['angle']['std'])
-        
-    def test_atoms_total_counts(self, topology):
-        assert len(topology.select_atoms("all")) == self.expected_n_atoms
-
-    def test_residues_total_counts(self, topology):
-        assert len(topology.select_atoms("all")) == self.expected_n_atoms
-
-    def test_segments_total_counts(self, topology):
-        assert len(topology.select_atoms("all")) == self.expected_n_atoms
+    def test_segments_total_counts(self, top):
+        assert len(top.select_atoms("all")) == self.expected_n_atoms
         
 
 class TestOpenBabelParserAtomBuild(OpenBabelParserBase):
 
-    @pytest.fixture(scope='class')
+    @pytest.fixture(autouse=True)
     def filename(self):
         return OBMol()
 
@@ -105,63 +92,46 @@ class TestOpenBabelParserAtomBuild(OpenBabelParserBase):
     expected_n_segments = 1
     expected_n_bonds = 1
 
-    @pytest.fixture(scope='class')
-    def parser(self, mol):
-        topology = OBParser(mol, **self.kwargs)
-        return topology
+    # @pytest.fixture(scope='class')
+    # def parser(self, mol):
+    #     top = OBParser(mol, **self.kwargs)
+    #     return top
 
-    def test_hbond_analysis(self, h):
 
-        assert len(np.unique(h.results.hbonds[:, 0])) == 10
-        assert len(h.results.hbonds) == 32
-
-        reference = {
-            'distance': {'mean': 2.7627309, 'std': 0.0905052},
-            'angle': {'mean': 158.9038039, 'std': 12.0362826},
-        }
-
-        assert_allclose(np.mean(h.results.hbonds[:, 4]),
-                        reference['distance']['mean'])
-        assert_allclose(np.std(h.results.hbonds[:, 4]),
-                        reference['distance']['std'])
-        assert_allclose(np.mean(h.results.hbonds[:, 5]),
-                        reference['angle']['mean'])
-        assert_allclose(np.std(h.results.hbonds[:, 5]),
-                        reference['angle']['std'])
         
-mol = OBMol()
-print(mol.NumAtoms()) #Should print 0 (atoms)
+# mol = OBMol()
+# print(mol.NumAtoms()) #Should print 0 (atoms)
 
-a = mol.NewAtom()
-a.SetAtomicNum(6)   # carbon atom
-a.SetVector(0.0, 1.0, 2.0) # coordinates
-b = mol.NewAtom()
-mol.AddBond(1, 2, 1)   # atoms indexed from 1
-print(mol.NumAtoms()) # Should print 2 (atoms)
-print(mol.NumBonds()) # Should print 1 (bond)
+# a = mol.NewAtom()
+# a.SetAtomicNum(6)   # carbon atom
+# a.SetVector(0.0, 1.0, 2.0) # coordinates
+# b = mol.NewAtom()
+# mol.AddBond(1, 2, 1)   # atoms indexed from 1
+# print(mol.NumAtoms()) # Should print 2 (atoms)
+# print(mol.NumBonds()) # Should print 1 (bond)
 
-for i in range(1, mol.NumAtoms()+1):
-    atom = mol.GetAtomById(i-1)
-    print(a)
+# for i in range(1, mol.NumAtoms()+1):
+#     atom = mol.GetAtomById(i-1)
+#     print(a)
 
-# --------
+# # --------
 
-obConversion = OBConversion()
-obConversion.SetInAndOutFormats("smi", "mdl")
+# obConversion = OBConversion()
+# obConversion.SetInAndOutFormats("smi", "mdl")
 
-mol = OBMol()
-obConversion.ReadString(mol, "C1=CC=CS1")
+# mol = OBMol()
+# obConversion.ReadString(mol, "C1=CC=CS1")
 
-#readfile(format="smi", filename="")
+# #readfile(format="smi", filename="")
 
-print(mol.NumAtoms()) # Should print 5 (atoms)
+# print(mol.NumAtoms()) # Should print 5 (atoms)
 
-mol.AddHydrogens()
-print(mol.NumAtoms()) # Should print 9 (atoms) after adding hydrogens
+# mol.AddHydrogens()
+# print(mol.NumAtoms()) # Should print 9 (atoms) after adding hydrogens
 
-outMDL = obConversion.WriteString(mol)
-print(outMDL)
+# outMDL = obConversion.WriteString(mol)
+# print(outMDL)
 
-# --------
+# # --------
 
 
